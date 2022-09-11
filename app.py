230ad19fdbda4e1c4ff60e1f16a35fac1ef01576
BadalSaini <111984530+BadalSaini@users.noreply.github.com>
2022-09-11 18:55:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import joblib
 #load the joblib model
 model_nb = joblib.load('Calories-burnt')
 
-st.title('SPAM HAM CLASSIFIER')
+st.title('PRIDICTION OF CALORIES BURNT DURING EXERCISE')
 ip = st.text_input('Enter your text :')
 
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])
-  
-           
